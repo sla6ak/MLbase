9e1f98c587501a5379a8e8555cc448631920a41a
sla6ak <slabakxaker@gmail.com>
2024-03-19 08:24:25 +0200

    fixed ilao

--- src/app/ilao/page.jsx
+++ src/app/ilao/page.jsx
@@ -1,9 +1,9 @@
-import Board from '@/components/board/Board'
+// import Board from '@/components/board/Board'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center p-6">
       <h1 className="mb-6">Hello, Ilao Page!</h1>
-      <Board />
+      {/* <Board /> */}
     </main>
   )
 }
